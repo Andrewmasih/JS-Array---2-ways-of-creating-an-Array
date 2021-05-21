ae233cf86a5c37316ed2fcffd992b96baf90066c
Andrew Masih <armasih@outlook.com>
2021-05-21 17:15:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+ArrayConstructor = (1, 2, 3, 4, 5, 6, 7, 8, 9); // array constructor
+
+console.log(ArrayConstructor);
+
+LiteralNotation = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // by puttting all the words in the bracket seperted by commas
+console.log(LiteralNotation);
